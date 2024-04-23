b42913d10afb9328e4b083d951e2075f57f93c24
BhargabRC <99333346+BhargabRC@users.noreply.github.com>
2024-04-23 11:12:19 +0530

    fix

--- src/Pages/SingleProduct.jsx
+++ src/Pages/SingleProduct.jsx
@@ -1,4 +1,5 @@
 import '../Style/singleproduct.scss'
+import MyImage from "../components/MyImage";
 
 export const SingleProduct = () => {
 
@@ -12,63 +13,43 @@ export const SingleProduct = () => {
     image,
   } = SingleProduct;
 
-  const MyImage = ({ imgs = [{ url: "" }] }) => {
-    const [mainImage, setMainImage] = useState(imgs[0]);
-
-    return (
 
 
-      <Container className="container">
-        <div className="grid grid-two-column">
-          {/* product Images  */}
-          <div className="product_images">
-            <div className="grid grid-four-column">
-              {imgs.map((curElm, index) => {
-                return (
-                  <figure>
-                    <img
-                      src={curElm.url}
-                      alt={curElm.filename}
-                      className="box-image--style"
-                      key={index}
-                      onClick={() => setMainImage(curElm)}
-                    />
-                  </figure>
-                );
-              })}
-            </div>
-            {/* 2nd column  */}
+  return (
 
-            <div className="main-screen">
-              <img src={mainImage.url} alt={mainImage.filename} />
-            </div>
-          </div>
 
-          {/* product dAta  */}
-          <div className="product-data">
-            <h2>{name}</h2>
-
-            <p className="product-data-price">
-              MRP:
-              <del>
-                <FormatPrice price={price + 250000} />
-              </del>
-            </p>
-            <p>{description}</p>
-
-            <div className="product-data-info">
-              <p>
-                ID : <span> {id} </span>
-              </p>
-            </div>
-            <hr />
-            <Button className="btn">Add To Cart</Button>
-          </div>
+    <Container className="container">
+      <div className="grid grid-two-column">
+        {/* product Images  */}
+        <div className="product_images">
+          <MyImage imgs={image} />
         </div>
-      </Container>
+
+        {/* product dAta  */}
+        <div className="product-data">
+          <h2>{name}</h2>
+
+          <p className="product-data-price">
+            MRP:
+            <del>
+              <FormatPrice price={price + 250000} />
+            </del>
+          </p>
+          <p>{description}</p>
+
+          <div className="product-data-info">
+            <p>
+              ID : <span> {id} </span>
+            </p>
+          </div>
+          <hr />
+          <Button className="btn">Add To Cart</Button>
+        </div>
+      </div>
+    </Container>
 
 
 
-    )
-  }
+  )
 }
+
