3d331511ce794a4706f99e1fa15ec16c6c509091
BhargabRC <99333346+BhargabRC@users.noreply.github.com>
2024-04-24 19:13:23 +0530

    fix: select color add: bg color change with select color left: image zoom

--- src/Components/Assets/all_product.js
+++ src/Components/Assets/all_product.js
@@ -3,38 +3,45 @@ import p2_img from "./product_2.png";
 import p3_img from "./product_3.png";
 import p4_img from "./product_4.png";
 import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
 
 
 let all_product = [
   {
     id: 1,
     name: "Sweet 1",
+    images: [p1_img, p3_img, p4_img, p5_img],
     image: p1_img,
     image1: p1_img,
     image2: p3_img,
     image3: p4_img,
     image4: p5_img,
+    image5: p6_img,
 
     price: 50.0,
   },
   {
     id: 2,
     name: "Sweet 2",
+    images: [p1_img, p3_img, p4_img, p5_img],
     image: p2_img,
     image1: p2_img,
     image2: p3_img,
     image3: p4_img,
     image4: p5_img,
+    image5: p6_img,
     price: 85.0,
   },
   {
     id: 3,
     name: "Sweet 3",
+    images: [p1_img, p3_img, p4_img, p5_img],
     image: p3_img,
     image1: p3_img,
     image2: p1_img,
     image3: p4_img,
     image4: p5_img,
+    image5: p6_img,
     price: 60.0,
   },
 ];
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -3,23 +3,6 @@ import { motion } from 'framer-motion';
 import './ProductDisplay.scss';
 
 const ProductDisplay = ({ product }) => {
-
-    useEffect(() => {
-        const activeImage = document.querySelector(".productdisplay-img .productdisplay-main-img");
-        const productImages = document.querySelectorAll(".productdisplay-img-list img");
-
-        function changeImage(e) {
-            activeImage.src = e.target.src;
-        }
-
-        productImages.forEach(image => image.addEventListener("click", changeImage));
-
-        return () => {
-            productImages.forEach(image => image.removeEventListener("click", changeImage));
-        };
-    }, [product]);
-
-
     const [mousePosition, setMousePosition] = useState({
         x: 0,
         y: 0,
@@ -42,6 +25,42 @@ const ProductDisplay = ({ product }) => {
         };
     }, []);
 
+
+    const [color, setColor] = useState("rgb(164, 90, 82)")
+    const click = color => {
+        setColor(color)
+    }
+    useEffect(() => {
+        document.body.style.backgroundColor = color;
+
+    }, [color]);
+
+
+    useEffect(() => {
+        const activeImage = document.querySelector(".productdisplay-img .productdisplay-main-img");
+        const activeGalleryImage = document.querySelector(".productdisplay-img-list .gallery1");
+        const productImages = document.querySelectorAll(".productdisplay-img-list img");
+        const productColorImages = document.querySelectorAll(".productdisplay-color-list img");
+
+        function changeImage(e) {
+            activeImage.src = e.target.src;
+        }
+
+        function changeGalleryImage(e) {
+            activeGalleryImage.src = e.target.src;
+        }
+
+        productImages.forEach(image => image.addEventListener("click", changeImage));
+        productColorImages.forEach(image => image.addEventListener("click", changeImage));
+        productColorImages.forEach(image => image.addEventListener("click", changeGalleryImage));
+
+        return () => {
+            productImages.forEach(image => image.removeEventListener("click", changeImage));
+            productColorImages.forEach(image => image.removeEventListener("click", changeImage));
+            productColorImages.forEach(image => image.removeEventListener("click", changeGalleryImage));
+        };
+    }, [product]);
+
     const imgEnter = () => setCursorVariant("img");
     const imgLeave = () => setCursorVariant("default");
 
@@ -60,6 +79,19 @@ const ProductDisplay = ({ product }) => {
         }
     };
 
+
+    // const [buttonColor, setButtonColor] = useState("rgb(164, 90, 82)");
+
+    // const handleColorChange = color => {
+    //     setButtonColor(color);
+    // }
+
+    // useEffect(() => {
+    //     document.body.style.backgroundColor = buttonColor;
+
+    // }, [buttonColor]);
+
+
     return (
         <div className='productdisplay'>
             <motion.div
@@ -69,10 +101,9 @@ const ProductDisplay = ({ product }) => {
             />
             <div className="productdisplay-left">
                 <div className="productdisplay-img-list">
-                    <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} src={product.image1} alt="" />
+                    <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} className='gallery1' src={product.image1} alt="" />
                     <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} src={product.image2} alt="" />
                     <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} src={product.image3} alt="" />
-                    <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} src={product.image4} alt="" />
                 </div>
                 <div className="productdisplay-img">
                     <img className='productdisplay-main-img' onMouseEnter={imgEnter} onMouseLeave={imgLeave} src={product.image} alt="" />
@@ -85,29 +116,21 @@ const ProductDisplay = ({ product }) => {
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel iste blanditiis itaque cum dignissimos. Voluptatem eos excepturi officiis tenetur, incidunt qui nisi quas, unde veniam veritatis debitis vel facilis! Impedit.
                 </div>
                 <div className="productdisplay-right-color">
-                    <h1>Color:</h1>
-                    <div className="productdisplay-right-colors">
-                        <div className="clearfix"></div>
-                        <input className="color-btn for-color-1" type="radio" id="color-1" name="color-btn" checked />
-                        <label className="first-color" htmlFor="color-1"></label>
-                        <input className="color-btn for-color-2" type="radio" id="color-2" name="color-btn" />
-                        <label className="color-2" htmlFor="color-2"></label>
-                        <input className="color-btn for-color-3" type="radio" id="color-3" name="color-btn" />
-                        <label className="color-3" htmlFor="color-3"></label>
-                        <div className="clearfix"></div>
+                    <h1>Select Color : </h1>
+                    <div className="color-groups">
+                        <div className="productdisplay-color-list">
+                            <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} onClick={() => click("rgb(164, 90, 82)")} src={product.image1} alt="" />
+                            <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} onClick={() => click("rgb(238, 232, 170)")} src={product.image4} alt="" />
+                            <img onMouseEnter={imgEnter} onMouseLeave={imgLeave} onClick={() => click("orange")} src={product.image5} alt="" />
+                        </div>
                     </div>
-                    <div className="info-wrap">
-                        <a href="#" className="btn"><i className="uil uil-shopping-cart icon"></i> Add To Cart</a>
+                    <div className="cart">
+                        <button type="button"  className="add">Add to Cart</button>
                     </div>
-                    <div className="img-wrap chair-1"></div>
-                    <div className="img-wrap chair-2"></div>
-                    <div className="img-wrap chair-3"></div>
-                    <div className="back-color"></div>
-                    <div className="back-color chair-2"></div>
-                    <div className="back-color chair-3"></div>
                 </div>
             </div>
         </div>
+
     );
 }
 
